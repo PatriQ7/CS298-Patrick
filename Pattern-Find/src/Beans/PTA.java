0a98b1d461d48d71d1499c971dae01424f3dbc38
Patrick Qi <qiqipat@gmail.com>
2016-03-11 20:40:00 -0800

    Update PTA.java

--- Pattern-Find/src/Beans/PTA.java
+++ Pattern-Find/src/Beans/PTA.java
@@ -3,6 +3,7 @@ package Beans;
 import Control.AlgAlergiaBFS;
 import Control.Config;
 import Util.StringUtil;
+import dfamini.*;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -257,9 +258,21 @@ public class PTA implements Serializable {
 
     
     private ArrayList nodearr;
+    private static ArrayList<node> node_list = new ArrayList<>();
     int nodenum = 1;
     private void preOrderTraverse_Rec(Node node, int depth, StringBuilder sb, int node_id) {
-        nodearr.add(node_id);
+        /*node this_node = new node(Integer.toString(node_id));
+        
+        for (Integer key : node.getChildren().keySet()) {
+        	node child = new node(Integer.toString(node.getChild(key).getNodeID()));
+        	this_node.children.put(Integer.toString(key), child);
+        }
+        	
+    	node_list.add(this_node);
+    	*/
+    	
+    	nodearr.add(node_id);
+    	
         //System.out.println(nodearr.toString());
         sb.append(StringUtil.space4Creator(depth));
 
